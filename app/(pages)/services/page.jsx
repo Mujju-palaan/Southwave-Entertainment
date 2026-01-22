83f05d3527ba5c8e55c6a336653aae33dd0f7354
Mujju-palaan <mdmujahid97@gmail.com>
2026-01-22 17:39:47 +0530

    service banner added

--- app/(pages)/services/page.jsx
+++ app/(pages)/services/page.jsx
@@ -2,10 +2,12 @@ import { Suspense } from "react";
 import ServicesClient from "../../../components/services/ServicePage";
 
 const Services = () => {
-  
-
   return (
     <Suspense fallback={<div>...Loading</div>}>
+      <div className="bg-[url('/service/southwave-entertainment-service-bannerr-events-london-uk.png')] 
+      bg-contain bg-center md:h-90 h-40">
+        {/* <!-- Content goes here --> */}
+      </div>
       <ServicesClient />
     </Suspense>
   );
